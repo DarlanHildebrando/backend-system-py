3a36d52f909d712f7b1d0df10079ec6ca4c20268
DarlanHildebrando <darlanmari41@gmail.com>
2025-10-27 14:39:41 -0300

    add MeView, configurando cors e integração com front realizada com sucesso!

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.urls import path
-from .views import LoginView, CookieJWTRefresh, LogoutView
+from .views import LoginView, CookieJWTRefresh, LogoutView, MeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('refresh/', CookieJWTRefresh.as_view(), name='refresh'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('me/', MeView.as_view(), name='me')
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -93,6 +93,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
